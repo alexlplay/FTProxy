alexlplay/FTProxy

commit 114d223c4f86bd0b0f33ed0ac662dc92f2c888e6

Basic MDTM support FSObject now stores a time.Time, not a string

parseindex/nginx.go
@@ -46,12 +46,18 @@ func ParseNginxHtmlList(r io.Reader) ([]FsObject) {
                 fmt.Println("No date and size regex match")
             } else {
                 // match[1] should contain date and time, match[2] size in bytes
-                tim, _ := time.Parse("_2-Jan-2006 15:04", match[1])
-                if tim.Year() < time.Now().Year() {
+                tim, err := time.Parse("_2-Jan-2006 15:04", match[1])
+                if err == nil {
+                    curObj.time = tim
+                } else {
+                    // Failed to parse time
+                    curObj.time = time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+                }
+                /* if tim.Year() < time.Now().Year() {
                     curObj.time = tim.Format("Jan _2 2006")
                 } else {
                     curObj.time = tim.Format(time.Stamp)
-                }
+                } */
                 if match[2] == "-" {
                     curObj.size = 0
                     curObj.otype = FS_DIR

parseindex/parseindex.go
@@ -18,17 +18,19 @@ const (
 type FsObject struct {
     otype FsObjectType
     name string
-    time string
+    time time.Time
     size int64
 }
 
 func GenDirList(objects []FsObject) (string) {
     var listing string
     for _, object := range objects {
+        printTime := object.time.Format(time.Stamp)
         if object.otype == FS_DIR {
-            listing = fmt.Sprintf("%sdrwxr-xr-x 1 0 0 1 %s %s\r\n", listing, object.time, object.name)
+            listing = fmt.Sprintf("%sdrwxr-xr-x 1 0 0 1 %s %s\r\n", listing, printTime, object.name)
+
         } else if object.otype == FS_FILE {
-            listing = fmt.Sprintf("%s-rwxr-xr-x 1 0 0 %d %s %s\r\n", listing, object.size, object.time, object.name)
+            listing = fmt.Sprintf("%s-rwxr-xr-x 1 0 0 %d %s %s\r\n", listing, object.size, printTime, object.name)
         }
     }
     return listing
@@ -45,6 +47,7 @@ func getTokenAttr(tok *html.Token, attrName string) (string) {
 
 type Parser interface {
     Parse(path string) (string, bool)
+    Mdtm(dirPath string, fileName string) (string, bool)
 }
 
 type ParserAutoIndex struct {
@@ -83,4 +86,40 @@ func (p ParserConf) Parse(truc string) (string, bool) {
         listing = fmt.Sprintf("%sdrwxr-xr-x 1 0 0 1 %s %s\r\n", listing, time.Now().Format(time.Stamp), path)
     }
     return listing, true
+}
+
+/* As with DIR, this assumes we look for a file in the current directory(dirPath).
+   fileName must be a single file name. Proper directory handling TBD */
+func (p ParserAutoIndex) Mdtm(dirPath string, fileName string) (string, bool) {
+    /* Whole section below similar to Parse(), factor it in a separate function */
+    cfg.LoadConfig("ftproxy.conf")
+    fmt.Printf("MDTM for file: -%s-\n", fileName)
+    vhost := cfg.GetVhost(dirPath)
+    url := fmt.Sprintf("http://%s/%s/", vhost, dirPath)
+    fmt.Printf("URL FOR INDEX: %s\n", url)
+    resp, err := http.Get(url)
+    if err != nil {
+        return "", false
+    }
+    defer resp.Body.Close()
+    var objects []FsObject
+    fmt.Printf("Server header: %s\n", resp.Header["Server"][0])
+    if strings.Contains(resp.Header["Server"][0], "nginx") {
+        objects = ParseNginxHtmlList(resp.Body)
+    } else {
+        objects = ParseApacheHtmlList(resp.Body)
+    }
+    /* End section*/
+    for _, object := range objects {
+        if object.name == fileName && object.otype == FS_FILE {
+            fmt.Printf("Found file, time is: %s\n", object.time)
+            mdtmTime := object.time.Format("20060102030405")
+            return mdtmTime, true
+        }
+    }
+    return "", false
+}
+
+func (p ParserConf) Mdtm(dirPath string, fileName string) (string, bool) {
+    return "", false
 }

ftproxy/ftproxy.go
@@ -118,6 +118,7 @@ func handleRequest(conn net.Conn) {
         "CWD":  cmdCwd,
         "LIST": cmdList,
         "FEAT": cmdFeat,
+        "MDTM": cmdMdtm,
     }
 
     scanner := bufio.NewScanner(conn)
@@ -131,7 +132,7 @@ func handleRequest(conn net.Conn) {
     go ctrlTimeout(&session)
 
     for scanner.Scan() {
-        session.timer.Reset(time.Second * 60)
+        session.timer.Reset(time.Second * 60 * 3)
         // Should have a limit on line length
         fmt.Println(session)
         line := scanner.Text()
@@ -424,5 +425,26 @@ func cmdFeat(session *Session, command Command) (bool) {
     featReply := "211-Features:\r\n MDTM\r\n211 End\r\n"
 
     ftpcmd.WriteRaw(session.commandConn, featReply)
+    return true
+}
+
+func cmdMdtm(session *Session, command Command) (bool) {
+    fileName := command.Args
+    var fileTime string
+    var ret bool
+    if session.workingDir == "/" {
+        fmt.Println("Mdtm (top dir parser)")
+        fileTime, ret = session.topDirParser.Mdtm(session.workingDir, fileName)
+    } else {
+        fmt.Println("Mdtm (default parser)")
+        fileTime, ret = session.defaultDirParser.Mdtm(session.workingDir, fileName)
+    }
+    if ret != true {
+        ftpcmd.Write(session.commandConn, 550, "Could not get file modification time.")
+        return false
+    }
+
+    ftpcmd.Write(session.commandConn, 213, fileTime)
+
     return true
 }

commit 289cbced5c53545a7aeee1ac5e138ca57550653e

Fix regression introduced in 114d223
(fix year display for dates not relative to current year)

parseindex/nginx.go
@@ -53,11 +53,6 @@ func ParseNginxHtmlList(r io.Reader) ([]FsObject) {
                     // Failed to parse time
                     curObj.time = time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
                 }
-                /* if tim.Year() < time.Now().Year() {
-                    curObj.time = tim.Format("Jan _2 2006")
-                } else {
-                    curObj.time = tim.Format(time.Stamp)
-                } */
                 if match[2] == "-" {
                     curObj.size = 0
                     curObj.otype = FS_DIR

parseindex/parseindex.go
@@ -25,10 +25,14 @@ type FsObject struct {
 func GenDirList(objects []FsObject) (string) {
     var listing string
     for _, object := range objects {
-        printTime := object.time.Format(time.Stamp)
+        var printTime string
+        if(object.time.Year() < time.Now().Year()) {
+            printTime = object.time.Format("Jan _2 2006")
+        } else {
+            printTime = object.time.Format(time.Stamp)
+        }
         if object.otype == FS_DIR {
             listing = fmt.Sprintf("%sdrwxr-xr-x 1 0 0 1 %s %s\r\n", listing, printTime, object.name)
-
         } else if object.otype == FS_FILE {
             listing = fmt.Sprintf("%s-rwxr-xr-x 1 0 0 %d %s %s\r\n", listing, object.size, printTime, object.name)
         }